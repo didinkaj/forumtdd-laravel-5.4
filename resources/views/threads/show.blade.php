206b5cd44db132022de8459839270f8a33c2fadb
Johnson Didinya <jdidinya@Cytonns-MacBook-Pro-2.local>
2018-07-21 13:14:27 +0300

    end of episode 3

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -21,7 +21,9 @@
                 @foreach($thread->replies as $reply)
                     <div class="panel panel-default">
                         <div class="panel-heading">
+                            <a href="{{$reply->owner->name}}">
                             {{$reply->owner->name}} said {{$thread->created_at->diffForHumans()}}
+                            </a>
                         </div>
                         <div class="panel-body">
                             {{$reply->body}}
