fedc2c9966839588790189f54590c3ab0bda78b1
Johnson Didinya <jdidinya@Cytonns-MacBook-Pro-2.local>
2018-08-11 16:37:04 +0300

    Extracting to View Composers eps13

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -11,16 +11,32 @@
                        <form action="/threads" method="POST" >
                            {{csrf_field()}}
                            <div class="form-group">
+                               <label for="channel_id">Choose a Channel</label>
+                               <select type="text" name="channel_id" class="form-control" id="channel_id" required>
+                                   <option value="">Choose one...</option>
+                                   @foreach($channels as $channel)
+                                     <option value="{{$channel->id}}" {{old('channel_id') == $channel->id ? 'selected' :'' }}>{{$channel->name}}</option>
+                                    @endforeach
+                               </select>
+                           </div>
+                           <div class="form-group">
                                <label for="title">Title</label>
-                               <input type="text" name="title" class="form-control" id="title" placeholder="title">
+                               <input type="text" name="title" class="form-control" id="title" placeholder="title" value="{{old('title')}}" required>
                            </div>
                            <div class="form-group">
                                <lable for="body">Body</lable>
-                               <textarea name="body" id="body" class="form-control"  rows="8"></textarea>
+                               <textarea name="body" id="body" class="form-control"  rows="8" required>{{old('body')}}</textarea>
                            </div>
                            <button type="submit" class="btn btn-default">Publish</button>
 
                        </form>
+                        @if(count($errors))
+                            <ul class="alert alert-danger">
+                                @foreach($errors->all() as $error)
+                                    <li>{{$error}}</li>
+                                @endforeach
+                            </ul>
+                            @endif
                     </div>
                 </div>
             </div>
