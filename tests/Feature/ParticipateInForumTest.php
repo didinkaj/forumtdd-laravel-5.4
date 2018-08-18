993830c6452bf69dc51196782507bc572771109f
Johnson Didinya <jdidinya@Cytonns-MacBook-Pro-2.local>
2018-08-19 00:47:30 +0300

    A User Can Filter All Threads By Username ep14

--- tests/Feature/ParticipateInForumTest.php
+++ tests/Feature/ParticipateInForumTest.php
@@ -10,48 +10,50 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 class ParticipateInForumTest extends TestCase
 {
     use DatabaseMigrations;
-
+    
     protected $thread;
-
+    
     public function setUp()
     {
         parent::setUp();
-
+        
         $this->thread = create('App\Thread');
-
+        
     }
-    /** @test*/
+    
+    /** @test */
     function unauthenticated_users_may_not_add_replies()
     {
         $this->withExceptionHandling()
-            ->post( 'threads/some-channel/1/replies', [])
+            ->post('threads/some-channel/1/replies', [])
             ->assertRedirect('/login');
     }
-
-   
+    
+    /** @test */
     function an_authenticated_user_may_participate_in_forum_threads()
     {
         $this->signIn();
-
-        $reply = factory('App\Reply')->make();
-
-        $this->post($this->thread->path(), $reply->toArray());
-
-        $this->get($this->thread->path())
-            ->assertSee($reply->body);
-
+        
+        $thread = create('App\Thread');
+        
+        $reply = make('App\Reply');
+        
+        $this->post($thread->path() . '/replies', $reply->toArray());
+        
+        $this->assertDatabaseHas('replies', ['body' => $reply->body]);
+        
     }
-
+    
     /** @test */
     function a_reply_requires_a_body()
     {
         $this->withExceptionHandling()->signIn();
-
+        
         $thread = create('App\Thread');
-
+        
         $reply = make('App\Reply', ['body' => null]);
-
+        
         $this->post($thread->path() . '/replies', $reply->toArray())
-                ->assertSessionHasErrors('body');
+            ->assertSessionHasErrors('body');
     }
 }
--- tests/Feature/ReadThreadTest.php
+++ tests/Feature/ReadThreadTest.php
@@ -40,16 +40,26 @@ class ReadThreadTest extends TestCase
             ->assertSee($this->thread->title);
 
     }
-   
+    /** test */
     public function user_can_read_replies_that_are_associated_with_a_thread()
     {
         $reply = factory('App\Reply')
             ->create(['thread_id' => $this->thread->id]);
 
-        $this->get($this->thread->path())
+        $this->get('/threads/some-channel/'.$this->thread->id)
             ->assertSee($reply->body);
 
 
+    }
+    /** @test */
+    function a_user_can_filter_threads_by_any_username()
+    {
+        $this->signIn(create('App\User', ['name' => 'JohnDoe']));
+        $threadByJohn = create('App\Thread', ['user_id' => auth()->id()]);
+        $threadNotByJohn = create('App\Thread');
+        $this->get('threads?by=JohnDoe')
+            ->assertSee($threadByJohn->title)
+            ->assertDontSee($threadNotByJohn->title);
     }
     /** @test */
     function a_user_can_filter_threads_according_to_a_channel()
@@ -57,13 +67,12 @@ class ReadThreadTest extends TestCase
         $channel = create("App\Channel");
 
         $threadInChannel = create('App\Thread', ['channel_id' => $channel->id]);
-        
 
         $threadNotInChannel = create('App\Thread');
 
         $this->get('/threads/'. $channel->slug)
             ->assertSee($threadInChannel->title)
             ->assertDontSee($threadNotInChannel->title);
-
     }
+    
 }
