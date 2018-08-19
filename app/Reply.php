f22a681b8d14ea584d69e60626cab1ea3ec82776
Johnson Didinya <jdidinya@Cytonns-MacBook-Pro-2.local>
2018-08-19 16:58:05 +0300

    A User Can Favorite Replies eps18

--- app/Reply.php
+++ app/Reply.php
@@ -13,5 +13,19 @@ class Reply extends Model
     {
         return $this->belongsTo(User::class,'user_id');
     }
+    public function favorites()
+    {
+        return $this->morphMany(Favorite::class, 'favorited');
+    }
+    public function favorite()
+    {
+        $attributes = ['user_id' => auth()->id()];
+        
+        if(!$this->favorites()->where($attributes)->exists())
+        {
+            $this->favorites()->create($attributes);
+        }
+       
+    }
 
 }
