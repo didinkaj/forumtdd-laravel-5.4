9ec036c7b18391f6bb090869b3cdedbff9dd0079
Johnson Didinya <jdidinya@Cytonns-MacBook-Pro-2.local>
2018-08-26 21:37:19 +0300

    This Thread Has Been Updated Since You Last Read It eps48

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -6,6 +6,7 @@ use App\Filters\ThreadFilters;
 use App\Channel;
 use App\Thread;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class ThreadsController extends Controller
@@ -83,6 +84,11 @@ class ThreadsController extends Controller
     public function show($channel, Thread $thread)
     {
         //
+        if (auth()->check())
+        {
+            auth()->user()->read($thread);
+        }
+        
         Return view('threads.show', compact('thread'));
     }
     
