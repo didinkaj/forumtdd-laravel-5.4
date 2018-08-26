3c19a5c3001e0d90d97df8a54cc350a25ea21544
Johnson Didinya <jdidinya@Cytonns-MacBook-Pro-2.local>
2018-08-26 18:08:33 +0300

    Notification Fakes in a Nutshell eps47

--- tests/Unit/ThreadTest.php
+++ tests/Unit/ThreadTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Unit;
 
+use App\Notifications\ThreadWasUpdated;
+use Illuminate\Support\Facades\Notification;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
@@ -89,6 +91,23 @@ class ThreadTest extends TestCase
             'user_id' => 1
         ]);
         $this->assertCount(1, $this->thread->replies);
+        
+    }
+    /** @test */
+    public function a_thread_notifies_all_registered_subscribers_when_a_reply_is_added()
+    {
+        Notification::fake();
+    
+        $this->signIn()
+            ->thread
+            ->subscribe()
+            ->addReply([
+            'body' => 'Foobar',
+            'user_id' => 999
+        ]);
+        
+        Notification::assertSentTo(auth()->user(), ThreadWasUpdated::class);
+        
         
     }
     
